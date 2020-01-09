7691dd4b35a04ff708e2c0426a964c1a18a02bef
BarryDong <barry.dong@morefun.me>
2020-01-09 13:41:07 +0800

    部署到 GitHub Pages

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+yarn build
+
+# cd 到构建输出的目录下 
+cd dist
+
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f https://github.com/dongdong12138/vue_for_yaoyao.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/dongdong12138/vue_for_yaoyao.git master:gh-pages
+
+cd -
