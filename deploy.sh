3ae875d33f665c6aacf8ea08c854689a49a0e55c
BarryDong <barry.dong@morefun.me>
2020-01-09 14:03:39 +0800

    vue.config.js去掉dist路径 & deploy.sh增加部署命令注释

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+#部署命令
+bash deploy.sh
 
 # 当发生错误时中止脚本
 set -e
@@ -9,15 +10,9 @@ yarn build
 # cd 到构建输出的目录下 
 cd dist
 
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f https://github.com/dongdong12138/vue_for_yaoyao.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/dongdong12138/vue_for_yaoyao.git master:gh-pages
